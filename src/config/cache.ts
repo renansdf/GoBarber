5619672a0fef09d6269d9784ec5d7baa5d71c5be
renansdf <rnn.frts@gmail.com>
2020-08-07 14:58:29 -0300

    Setting up Heroku

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -14,7 +14,7 @@ export default {
 
   config: {
     redis: {
-      url: process.env.REDIS_URL,
+
       // host: process.env.REDIS_HOST,
       // port: process.env.REDIS_PORT,
       // password: process.env.REDIS_PASS || undefined,
