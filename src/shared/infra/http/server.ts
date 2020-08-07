c7c1e34bf0017419414f8de447783f4658a42526
renansdf <rnn.frts@gmail.com>
2020-08-07 14:10:03 -0300

    Setting up Heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -39,6 +39,6 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Servidor iniciado na porta 3333');
 });
